5ec107443a616c274ce8eeac544f0480d94c111d
JohnCritch90 <jcritch90@gmail.com>
2023-06-19 21:41:11 +0000

    blah

--- src/api/models.py
+++ src/api/models.py
@@ -9,11 +9,68 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class MentalHealthResources (db.Model):
+  resource_id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  description = db.Column(db.Text)
+  type = db.Column(db.String(255))
+  url = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'<MentalHealthResources {self.title}>'
+
+  def serialize(self):
+    return {
+      "resource_id": self.resource_id,
+      "title": self.title,
+      "description": self.description,
+      "type": self.type,
+      "url": self.url,
+    }
+
+class MeditationSessions (db.Model):
+  __tablename__ = 'meditation_sessions'
+  session_id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  style = db.Column(db.String(255))
+  theme = db.Column(db.String(255))
+  youtube_url = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'<MeditationSessions {self.title}>'
+
+  def serialize(self):
+    return {
+      "session_id": self.session_id,
+      "title": self.title,
+      "style": self.style,
+      "theme": self.theme,
+      "youtube_url": self.youtube_url
+    }
+
+class JournalEntries(db.Model):
+    __tablename__ = 'journal_entries'
+    entry_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(255), nullable=False)  # Change the column type to String
+    mood = db.Column(db.String(255))
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<JournalEntries {self.date}>'
+
+    def serialize(self):
+        return {
+            "entry_id": self.entry_id,
+            "date": self.date,
+            "mood": self.mood,
+            "content": self.content
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,102 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
+import os  
+from flask import Flask ,request, jsonify, Blueprint 
+# from flask_jwt_extended import create_access_token
+# from flask_jwt_extended import get_jwt_identity 
+# from flask_jwt_extended import jwt_required 
+from api.models import db, User, MentalHealthResources, JournalEntries
+import datetime
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def signup():
+    rb=request.get_json()
+    email = rb["email"]
+    password = rb["password"]
+    user=User.query.filter_by(email=email).first()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if user:
 
-    return jsonify(response_body), 200
+        return jsonify(message='Email already registered'), 200
+
+    new_user = User(email=rb["email"], password=rb["password"], is_active=True)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(message='User register successfully'), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(message=e), 200
+    
+@api.route('/login', methods=['POST'])
+def login():
+    get_login = request.get_json()
+    email = get_login["email"]
+    password = get_login["password"]
+
+    if not email or not password:
+        return jsonify({'message': 'Email and password are required'}), 400
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or user.password != password:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    # create token
+    # expiration = datetime.timedelta(days=3)
+    # access_token = create_access_token(identity= user.id, expires_delta= expiration)
+    # return jsonify({'message': 'Logged in successfully.', 'access_token':access_token}), 200
+
+    return jsonify ({'message': 'You have logged in successfully.'})
+
+@api.route('/resource', methods=['GET', 'POST'])
+def resource():
+    get_resource=request.get_json()
+    title=get_resource["title"]
+    description=get_resource["description"]
+    type=get_resource["type"]
+    url=get_resource["url"]
+    resource=MentalHealthResources.query.filter_by(title=title).first()
+
+    new_resource=MentalHealthResources(title=get_resource["title"])
+                                       
+    db.session.add(new_resource)
+    db.session.commit()
+    return jsonify(message='Your resource is ready.'), 200
+
+@api.route('/meditation', methods=['GET', 'POST'])
+def meditation():
+    get_session=request.get_json()
+    title=get_session["title"]
+    style=get_session["style"]
+    theme=get_session["theme"]
+    youtube_url=get_session["youtube_url"]
+    meditation=MeditationSessions.query.filter_by(title=title).first()
+
+    new_session=MeditationSessions(title=get_session["title"])
+                                       
+    db.session.add(new_session)
+    db.session.commit()
+    return jsonify(message='Your session is ready.'), 200
+
+@api.route('/journal', methods=['POST'])
+def journal_entries():
+    get_entry = request.get_json()
+    date = get_entry.get("date", None)
+    mood = get_entry.get("mood", None)
+    content = get_entry.get("content", None)
+
+    if not date:
+        return jsonify(message='Date is required'), 400
+
+    new_entry = JournalEntries(date=date, mood=mood, content=content)
+
+    try:
+        db.session.add(new_entry)
+        db.session.commit()
+        return jsonify(message='Your new entry is available'), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(message=str(e)), 500

0597fb71fdadc569fe84dc8cf1757e6811971ceb
r-moore98 <123704999+r-moore98@users.noreply.github.com>
2023-06-19 21:47:07 +0000

    did signup loginpage(home)

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+
+		<p>&copy; 2023 Mental Health Mate. All rights reserved.</p>
+
 	</footer>
 );
---
+++ src/front/js/pages/mainPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar } from "../component/navbar";
+
+import { ReactDOM } from "react-dom";
+
+
+
+
+export const MainPage = () => {
+    return (
+        <div className="Navbar">
+            <Navbar />
+            <div>
+
+                <p>MainPage</p>
+            </div>
+        </div>
+    );
+};

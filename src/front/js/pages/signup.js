aeff28e6f1285ac1c0ca35e41b8fda9b09ef3b05
r-moore98 <123704999+r-moore98@users.noreply.github.com>
2023-06-19 16:49:45 -0500

    did signup loginpage(home) (#1)

---
+++ src/front/js/pages/signup.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState } from "react";
+import { Button } from "react-bootstrap";
+import { Context } from "../store/appContext";
+
+export const Signup = () => {
+    const { store, actions } = useContext(Context);
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+
+    const token = sessionStorage.getItem; ("token");
+    console.log(token);
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(e.target)
+        actions.createUser(email, password)
+    }
+
+    return (
+        <div className="loginCont">
+            <form className="loginForm">
+                <div className="loginFormContent">
+                    <h1>Sign Up for account </h1>
+                    <div className="input-field">
+                        <input className="myInput" type={"text"} placeholder={'Email'} value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="input-field">
+                        <input className="myInput" type={'password'} placeholder={'Password'} value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+
+                </div>
+                <div className="loginFormAction">
+                    <button className="formBtn regBtn" onClick={(e) => handleClick(e)}>Register</button>
+                </div>
+            </form>
+        </div>
+    );
+}

84f142c3ab2c4b30b463a3a84dd4b37208b98f9d
r-moore98 <123704999+r-moore98@users.noreply.github.com>
2023-06-21 11:11:15 -0500

    Flux (#4)
    * added branch
    
    * did create and login
    
    * cleaned

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,134 +1,224 @@
-const axios = require('axios');
 const getState = ({ getStore, getActions, setStore }) => {
+  return {
+    store: {
+      token: null,
+      cf_url: 'https://3000-nchang007-fullstartwars-kwutb4wefk5.ws-us89.gitpod.io',
+      cb_url: 'https://3001-nchang007-fullstartwars-kwutb4wefk5.ws-us89.gitpod.io',
+      characters: [],
+      planets: [],
+      starships: [],
+      created: [],
+      favorites: [],
+      user_name: null,
+    },
+    actions: {
+      loadData: () => {
+        fetch("https://swapi.dev/api/people")
+          .then((res) => res.json())
+          .then((data) => {
+            setStore({ characters: data.results });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
-	return {
-		store: {
-			token: null,
-			cf_url: 'https://3000-4geeksacademy-mhm-xwbjejb9wmj.ws-us100.gitpod.io',
-			cb_url: 'https://3001-4geeksacademy-mhm-xwbjejb9wmj.ws-us100.gitpod.io',
-			condition: [],
-			video: [],
-		},
-		actions: {
-			// Use getActions to call a function within a function
-			getCondition: async () => {
-				axios.get('https://api.nhs.uk/conditions/', {
-					headers: {
-						'subscription-key': 'cc1c63174d5347d1ac10dd551d783a2f'
-					}
-				})
-					.then(response => {
-						const articles = response.data.articles;
-						console.log(articles);
-					})
-					.catch(error => {
-						console.error(error);
-						console.log({ error: 'Internal server error' });
-					});
-			},
+        fetch("https://swapi.dev/api/planets")
+          .then((res) => res.json())
+          .then((data) => {
+            setStore({ planets: data.results });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
-			getVideo: async () => {
-				const search = req.query.search;
-				axios
-					.get('https://www.googleapis.com/youtube/v3/search', {
-						params: {
-							q: search,
-							key: 'AIzaSyC2PlQqnTpfW5zcRsMGVabvkg31tZQesao'
-						}
-					})
-					.then(response => {
-						const videoId = response.data.items[0].id.videoId;
-						console.log({ videoId });
-					})
-					.catch(error => {
-						console.error(error);
-						console.log({ error: 'Internal server error' });
-					});
-			},
+        fetch("https://swapi.dev/api/starships")
+          .then((res) => res.json())
+          .then((data) => {
+            setStore({ starships: data.results });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
-			createUser: async (email, password) => {
-				const cb_url = getStore().cb_url;
-				const cf_url = getStore().cf_url;
-				const url = cb_url + "/api/signup";
-				const data = {
-					email: email,
-					password: password,
-				};
-				try {
-					const response = await axios.post(url, data, {
-						headers: {
-							"Content-type": "application/json",
-							"Assess-Control-Allow_Origin": "*"
-						},
-					});
+        fetch(cb_url + "/api/characters")
+          .then((res) => res.json())
+          .then((data) => {
+            setStore({ created: data.data });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        const cb_url = getStore().cb_url
+        //this is to update the favorites when the user logs in
+        const token = sessionStorage.getItem("token");
+        if (token) {
+          const opts = {
+            headers: {
+              Authorization: "Bearer " + token
+            },
+           // method: "POST"
+          };
+          fetch(cb_url + "/api/getfavorites",opts)
+            .then((res) => res.json())
+            .then((data) => {
+              setStore({ favorites: data.favorites });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      },
+      getCharacter: (idx) => {
+        const characters = getStore().characters;
+        return characters[idx];
+      },
+      getPlanet: (idx) => {
+        const planets = getStore().planets;
+        return planets[idx];
+      },
+      getStarship: (idx) => {
+        const starships = getStore().starships;
+        return starships[idx];
+      },
+      getCreated: (idx) => {
+        const created = getStore().created;
+        return created[idx];
+      },
 
-					if (response.status !== 200) {
-						alert("There has been an error");
-						return false;
-					}
+      syncTokenFromSessionStore: () => {
+        const token = sessionStorage.getItem("token");
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
 
-					const responseData = response.data;
+      logout: () => {
+        const cf_url = getStore().cf_url
+        const token = sessionStorage.removeItem("token");
+        setStore({ token: null });
+        window.location.href = cf_url + "/";
+      },
 
-					if (responseData.status === "true") {
-						window.location.href = cf_url + "/home"
-					}
+      login: async (email, password) => {
+        const cb_url = getStore().cb_url
+        const opts = {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
+        try {
+          const res = await fetch(cb_url + "/api/login", opts);
+          if (res.status !== 200) {
+            alert("there has been an error");
+            return false;
+          }
+          const data = await res.json();
+          sessionStorage.setItem("token", data.access_token);
+          data.favorites.forEach(f => {
+            //was returning an error bc it didnt like the single quotes so the line below turns the single into double quotes 
+            f.item = f.item.replace(/'/g, '"')
+            f.item = JSON.parse(f.item)
+          })
+          setStore({ token: data.access_token, favorites: data.favorites, user_name: data.user_name });
+          return true;
+        } catch (error) {console.error(error)}
+      },
 
-					return true;
-				} catch (error) {
-					console.error(error);
-				}
+      createUser: async (name, email, password) => {
+        const cb_url = getStore().cb_url
+        const cf_url = getStore().cf_url
+        const opts = {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+          }),
+        };
+        try {
+          const res = await fetch(cb_url + "/api/createUser", opts);
+          if (res.status !== 200) {
+            alert("there has been an error");
+            return false;
+          }
+          const data = await res.json();
+          // console.log(data);
+          if (data.status == "true") {
+            //redirect to login
+            window.location.href = cf_url + "/login" 
+          }
+          return true;
+        } catch (error) {console.error(error);}
+      },
 
-			},
+      addFavorite: (item) => {
+        const cb_url = getStore().cb_url
+        let f = getStore().favorites;
+        let t = getStore().token;
+        if (sessionStorage.getItem("token")) {
+          const opts = {
+            headers: {
+              Authorization: "Bearer " + t,
+			        'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(item),
+          };
+          fetch(cb_url + "/api/favorites", opts)
+            .then((response) => response.json())
+            .then((data) => {
+              if(data.msg == "ok") {
+                f.push(item);
+                setStore({ favorites: f });
+              }
+            })
+            .catch((error) => {console.log(error);});
+        }
+      },
+      removeFavorite: (item) => {
+        const cb_url = getStore().cb_url
+        let f = getStore().favorites;
+        let t = getStore().token;
+        if (sessionStorage.getItem("token")) {
+          const opts = {
+            headers: {
+              Authorization: "Bearer " + t,
+			        'Content-Type': 'application/json'
+            },
+            method: "DELETE",
+            body: JSON.stringify(item),
+          };
+          fetch(cb_url + "/api/deletefav", opts)
+            .then((response) => response.json())
+            .then((data) => {
+              if(data.msg == "ok") {
+                f.forEach((el, i) => {
+                  if (el.id === item.id && el.type === item.type) {
+                    f.splice(i, 1);
+                  }
+                });
+                setStore({ favorites: f });
+              }
+            })
+            .catch((error) => {console.log(error);});
+        }
+      },
 
-			login: async (email, password) => {
-				const cb_url = getStore().cb_url;
-				const url = cb_url + "/api/login";
-				const data = {
-					email: email,
-					password: password,
-				};
 
-				try {
-					const response = await axios.post(url, data, {
-						headers: {
-							"Content-Type": "application/json",
-							"Access-Control-Allow-Origin": "*",
-						},
-					});
 
-					if (response.status !== 200) {
-						alert("There has been an error");
-						return false;
-					}
-
-					const responseData = response.data;
-					sessionStorage.setItem("token", responseData.access_token);
-					responseData.favorites.forEach(f => {
-						f.item = f.item.replace(/'/g, '"');
-						f.item = JSON.parse(f.item);
-					});
-
-					setStore({ token: responseData.access_token, favorites: responseData.favorites, user_name: responseData.user_name });
-					return true;
-				} catch (error) {
-					console.error(error);
-				}
-			},
-
-			getMessage: async () => {
-				try {
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
-					const data = await resp.json();
-					setStore({ message: data.message });
-					// don't forget to return something, that is how the async resolves
-					return data;
-				} catch (error) {
-					console.log("Error loading message from backend", error);
-				}
-			},
-		}
-	};
+    },
+  };
 };
 
-export default getState
+export default getState;

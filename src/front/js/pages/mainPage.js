ce12846cbfffda03d94651262e688f2d277c5cce
r-moore98 <123704999+r-moore98@users.noreply.github.com>
2023-06-23 18:14:02 -0500

    Flux (#8)
    * added branch
    
    * did create and login
    
    * cleaned
    
    * fixed signup and login
    
    * all login logout signup works
    
    ---------
    
    Co-authored-by: JohnCritch90 <123122544+JohnCritch90@users.noreply.github.com>

--- src/front/js/pages/mainPage.js
+++ src/front/js/pages/mainPage.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar } from "../component/navbar";
 
 import { ReactDOM } from "react-dom";
-
-
-
+import { Context } from "../store/appContext";
 
 export const MainPage = () => {
+    const { store, actions } = useContext(Context)
     return (
         <div className="Navbar">
             <Navbar />
             <div>
 
                 <p>MainPage</p>
+                <button
+                    onClick={actions.logout}
+                    className="btn btn-warning"
+                    style={{ margin: '3rem' }}
+                >
+                    Log Out
+                </button>
             </div>
         </div>
     );

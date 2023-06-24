9b53d9f4c08bf8537077cc19e70710cdf894b016
r-moore98 <123704999+r-moore98@users.noreply.github.com>
2023-06-24 00:37:43 +0000

    first youtube version

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { JournalApp } from "./component/journalapp.jsx";
 import { Home } from "./pages/home";
 import { Signup } from "./pages/signup";
 import { MainPage } from "./pages/mainPage";
+import { Meditation } from "./pages/meditation";
 import injectContext from "./store/appContext";
 
 
@@ -29,6 +30,7 @@ const Layout = () => {
                         <Route element={<Signup />} path="/signup" />
                         <Route element={<MainPage />} path="/mainpage" />
                         <Route element={<JournalApp />} path="/journal" />
+                        <Route element={<Meditation />} path="/meditation"/>
                     </Routes>
                     <Footer />
                 </ScrollToTop>
---
+++ src/front/js/pages/meditation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Meditation = () => {
+    return (
+        <><h1>Welcome to the meditation page</h1>
+            <h2>you can watch videos for relaxation by looking up videos through YouTube!</h2></>
+
+    );
+};

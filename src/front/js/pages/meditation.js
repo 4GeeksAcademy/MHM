c94161d65b70de6665bd2526326d9a6dc00ae390
r-moore98 <123704999+r-moore98@users.noreply.github.com>
2023-06-26 01:27:42 +0000

    started youtube page

--- src/front/js/pages/meditation.js
+++ src/front/js/pages/meditation.js
@@ -1,6 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 export const Meditation = () => {
+    const [condition, setCondition] = useState("");
+    const [result, setResult] = useState(null);
+    const [error, setError] = useState("");
+    const getVideo = () => {
+        axios
+            .get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    q: search,
+                    key: 'AIzaSyC2PlQqnTpfW5zcRsMGVabvkg31tZQesao'
+                }
+            })
+            .then(response => {
+                const videoId = response.data.items[0].id.videoId;
+                console.log({ videoId });
+            })
+            .catch(error => {
+                console.error(error);
+                console.log({ error: 'Internal server error' });
+            });
+    };
+
+
+
     return (
         <><h1>Welcome to the meditation page</h1>
             <h2>you can watch videos for relaxation by looking up videos through YouTube!</h2></>
